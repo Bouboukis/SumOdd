5f3e8f71319d7baeffbc5a4054e9b59969a2de67
gekac <37373492+Bouboukis@users.noreply.github.com>
2021-07-13 18:14:00 +0200

    1

---
+++ src/com/georgioskachrimanis/javacourse/Main.java
@@ -0,0 +1,9 @@
+package com.georgioskachrimanis.javacourse;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("The summary of odd numbers in rage 1 - 1.000 is " + SumOddRange.sumOdd(1, 100));
+    }
+}
---
+++ src/com/georgioskachrimanis/javacourse/README.md
@@ -0,0 +1,7 @@
+**Sum Odd**
+
+Program to add the odd numbers of a range.
+
+It is from this course.
+
+https://www.udemy.com/course/java-the-complete-java-developer-course/
